b8fb11c9f62100dfa2c10bb3aeafa96adacef424
Loz Jackson <lozjackson@talktalk.net>
2015-10-06 21:43:35 +0100

    update clock service test

--- tests/unit/services/clock-test.js
+++ tests/unit/services/clock-test.js
@@ -1,4 +1,5 @@
 import { moduleFor, test } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleFor('service:clock', 'Unit | Service | clock', {
   // Specify the other units that are required for this test.
@@ -9,4 +10,40 @@ moduleFor('service:clock', 'Unit | Service | clock', {
 test('it exists', function(assert) {
   var service = this.subject();
   assert.ok(service);
+});
+
+/*
+  check that the clock starts and keeps time after 10 seconds
+*/
+test('clock ticks 10 seconds', function(assert) {
+
+  assert.expect(6);
+  var clock = this.subject();
+  var date = new Date();
+  window.QUnit.stop();
+  assert.equal(clock.get('second'), date.getSeconds());
+  assert.equal(clock.get('minute'), date.getMinutes());
+  assert.equal(clock.get('hour'), date.getHours());
+
+  Ember.run.later(function() {
+    var date = new Date();
+    assert.equal(clock.get('second'), date.getSeconds());
+    assert.equal(clock.get('minute'), date.getMinutes());
+    assert.equal(clock.get('hour'), date.getHours());
+    window.QUnit.start();
+  }, 10050);
+});
+
+/*
+  check that the clock stops
+*/
+test('clock stops', function(assert) {
+
+  assert.expect(2);
+  var clock = this.subject();
+  var date = new Date();
+  assert.equal(clock.get('second'), date.getSeconds());
+
+  clock.stopClock();
+  assert.equal(clock.get('nextTick', null));
 });
