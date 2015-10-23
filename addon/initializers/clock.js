b22458e13cc3e8ed35e51080fc8f944c5a6a995b
Loz Jackson <lozjackson@talktalk.net>
2015-10-23 08:19:14 +0100

    update clock initializer
    make initializer function work with Ember 2.0 and 2.1

--- addon/initializers/clock.js
+++ addon/initializers/clock.js
@@ -1,7 +1,8 @@
 /*
   Inject the clock service into all controllers and components
 */
-export function initialize( app ) {
+export function initialize() {
+  const app = arguments[1] || arguments[0];
   app.inject('controller', 'clock', 'service:clock');
   app.inject('component', 'clock', 'service:clock');
 }
