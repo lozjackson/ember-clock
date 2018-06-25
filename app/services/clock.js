fc49ae1de05f2370261b4fd1bde005fd5005a2c6
Richard Bezemer <richard@bezemer.ca>
2018-06-25 11:47:17 -0600

    Making ENV accessible to clock service

--- app/services/clock.js
+++ app/services/clock.js
@@ -1,1 +1,5 @@
-export { default } from 'ember-clock/services/clock';
+import ENV from '../config/environment';
+import Service from 'ember-clock/services/clock'
+export default Service.extend({
+  disabled: ENV['ember-clock'] && ENV['ember-clock'].disabled
+})
