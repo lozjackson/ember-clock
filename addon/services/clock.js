1af8305c2f3b1e297fdb8fa0f7dc63dfe84d7cb9
Loz Jackson <lozjackson@users.noreply.github.com>
2024-05-04 12:44:44 +0100

    use modern syntax

--- addon/services/clock.js
+++ addon/services/clock.js
@@ -1,10 +1,10 @@
 /**
-  @module ember-clock
-*/
-import { bool } from '@ember/object/computed';
-
-import { run } from '@ember/runloop';
+ * @module ember-clock
+ */
 import Service from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import { bool } from '@ember/object/computed';
+import { cancel, later } from '@ember/runloop';
 
 /**
   ## ClockService
@@ -35,25 +35,24 @@ import Service from '@ember/service';
   @class ClockService
   @namespace EmberClock
 */
-export default Service.extend({
-
+export default class ClockService extends Service {
   /**
     @property hour
     @type {Integer}
   */
-  hour: null,
+  @tracked hour = null;
 
   /**
     @property minute
     @type {Integer}
   */
-  minute: null,
+  @tracked minute = null;
 
-   /**
+  /**
     @property second
     @type {Integer}
   */
-  second: null,
+  @tracked second = null;
 
   /**
     Stores the next tick, so that it can be cancelled and the clock stopped.
@@ -61,7 +60,7 @@ export default Service.extend({
     @type {Object}
     @private
   */
-  nextTick: null,
+  @tracked nextTick = null;
 
   /**
     @property isTicking
@@ -69,17 +68,17 @@ export default Service.extend({
     @readonly
     @private
   */
-  isTicking: bool('nextTick'),
+  @bool('nextTick') isTicking;
 
   /**
     Call `start()`
     @method init
     @private
   */
-  init() {
-    this._super(...arguments);
+  constructor() {
+    super(...arguments);
     this.start();
-  },
+  }
 
   /**
     Start the clock
@@ -88,7 +87,7 @@ export default Service.extend({
   */
   start() {
     this.tick();
-  },
+  }
 
   /**
     Stop the clock
@@ -96,9 +95,9 @@ export default Service.extend({
     @private
   */
   stop() {
-    run.cancel(this.get('nextTick'));
-    this.set('nextTick', null);
-  },
+    cancel(this.nextTick);
+    this.nextTick = null;
+  }
 
   /**
     Set the time to the current time.
@@ -106,13 +105,11 @@ export default Service.extend({
     @private
   */
   setTime() {
-    let now = new Date();
-    this.setProperties({
-      second: now.getSeconds(),
-      minute: now.getMinutes(),
-      hour:   now.getHours()
-    });
-  },
+    const now = new Date();
+    this.second = now.getSeconds();
+    this.minute = now.getMinutes();
+    this.hour = now.getHours();
+  }
 
   /**
     Ticks the clock
@@ -121,11 +118,11 @@ export default Service.extend({
   */
   tick() {
     this.setTime();
-    if (this.get('disabled')) {
+    if (this.disabled) {
       return;
     }
-    this.set('nextTick', run.later(this, this.tick, 1000));
-	},
+    this.nextTick = later(this, this.tick, 1000);
+  }
 
   /**
     call `stop()`
@@ -135,4 +132,4 @@ export default Service.extend({
   willDestroy() {
     this.stop();
   }
-});
+}
--- app/services/clock.js
+++ app/services/clock.js
@@ -2,6 +2,7 @@ import ENV from '../config/environment';
 import Service from 'ember-clock/services/clock'
 
 const isDisabled = ENV['ember-clock'] && ENV['ember-clock'].disabled;
-export default Service.extend({
-  disabled: isDisabled
-})
+
+export default class ClockService extends Service {
+  disabled = isDisabled
+}
