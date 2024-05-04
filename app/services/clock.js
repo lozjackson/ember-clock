e2bb9f181030534033a4c3de1e8ae6541749a020
Loz Jackson <lozjackson@users.noreply.github.com>
2024-05-04 14:08:05 +0100

    Update clock.js

--- app/services/clock.js
+++ app/services/clock.js
@@ -1,8 +1,8 @@
 import ENV from '../config/environment';
-import Service from 'ember-clock/services/clock'
+import Service from 'ember-clock/services/clock';
 
 const isDisabled = ENV['ember-clock'] && ENV['ember-clock'].disabled;
 
 export default class ClockService extends Service {
-  disabled = isDisabled
+  disabled = isDisabled;
 }
