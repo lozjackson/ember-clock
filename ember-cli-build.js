9c418e6f5991fe2e5a1a5596ec5c7cdcc9b5d870
Loz Jackson <lozjackson@users.noreply.github.com>
2024-05-04 14:07:53 +0100

    remove ember-code-snippet

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,8 +4,7 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function (defaults) {
   const app = new EmberAddon(defaults, {
-    snippetPaths: ['tests/dummy/snippets'],
-    snippetSearchPaths: ['tests/dummy/app']
+    // Add options here
   });
 
   /*
